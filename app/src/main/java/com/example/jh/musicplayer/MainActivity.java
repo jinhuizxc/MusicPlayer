6529dbe10113366c047c12654db92fd288408b06
jinhuizxc <1004260403@qq.com>
2017-06-06 09:51:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/musicplayer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 打造一款音乐播放器，借鉴ponyMusic
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
